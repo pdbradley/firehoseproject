06484aa547117ad87ed34c14969d4f5341fda2cf
Philip Bradley <pdbradley@gmail.com>
2016-04-13 22:34:11 -0400

    some simple stack tests

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'linked_list_node'
 require_relative 'stack'
+require 'pry'
 
 describe LinkedListNode do
   it "should be printable" do
@@ -7,17 +8,37 @@ describe LinkedListNode do
 
     expect(node123.to_s).to eq "1 --> 2 --> 3 --> nil\n"
   end
+end
 
+describe Stack do
+  it "should allow for pushing elements onto it" do
+    stack = Stack.new
 
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    expect(stack.to_s).to eq "3 --> 2 --> 1 --> nil\n"
+  end
+
+  it "should allow for popping pushed elements from it" do
+    stack = Stack.new
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    popped = stack.pop
+
+    expect(popped).to eq 3
+    expect(stack.to_s).to eq "2 --> 1 --> nil\n"
+  end
+end
 
 
   # helper methods
-
   def linked_list_1_through_3
     node1 = LinkedListNode.new(3)
     node2 = LinkedListNode.new(2, node1)
     node3 = LinkedListNode.new(1, node2)
     node3
   end
-
-end
--- stack.rb
+++ stack.rb
@@ -7,14 +7,24 @@ class Stack
 
     # Push an item onto the stack
     def push(element)
-      # IMPLEMENT ME!
+      element_node = LinkedListNode.new(element)
+      element_node.next_node = @data if @data
+      @data = element_node
     end
 
     # Pop an item off the stack.  
     # Remove the last item that was pushed onto the
     # stack and return it to the user
     def pop
-      # IMPLEMENT ME
+      popped = @data.value
+      @data = @data.next_node
+      popped
+    end
+
+
+    # convenience print method
+    def to_s
+      @data.to_s
     end
 
 end
