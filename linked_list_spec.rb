067f5b11c60f72a8d41e9e5e5946ca4db92345ee
Philip Bradley <pdbradley@gmail.com>
2016-04-13 22:21:44 -0400

    first basic test passing

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,3 +1,22 @@
-describe Stack do
+require_relative 'linked_list_node'
+
+describe LinkedListNode do
+  it "should be printable" do
+    node123 = linked_list_1_through_3
+
+    expect(node123.to_s).to eq "1 --> 2 --> 3 --> nil\n"
+  end
+
+
+
+
+  # helper methods
+
+  def linked_list_1_through_3
+    node1 = LinkedListNode.new(3)
+    node2 = LinkedListNode.new(2, node1)
+    node3 = LinkedListNode.new(1, node2)
+    node3
+  end
 
 end
